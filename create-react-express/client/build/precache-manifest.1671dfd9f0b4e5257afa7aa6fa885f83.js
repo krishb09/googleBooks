c7b9809e9b05b59dc464b387eba59e34e2dcc545
Krish <aalsbhowmick@gmail.com>
2019-11-19 20:38:47 -0500

    build

---
+++ create-react-express/client/build/precache-manifest.1671dfd9f0b4e5257afa7aa6fa885f83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b508a453a3b3231832c4",
+    "url": "/static/js/main.b8e9e446.chunk.js"
+  },
+  {
+    "revision": "0d7f2d4f650486126da3",
+    "url": "/static/js/2.e5a867c7.chunk.js"
+  },
+  {
+    "revision": "b508a453a3b3231832c4",
+    "url": "/static/css/main.15783425.chunk.css"
+  },
+  {
+    "revision": "e3b1945307bb863f20a8ae0d25d880a1",
+    "url": "/index.html"
+  }
+];
