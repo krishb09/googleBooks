dd18e53d1d0992b280fe5aff60a5017aad16a99b
Krish <aalsbhowmick@gmail.com>
2019-11-21 19:48:08 -0500

    Change public folder path

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("create-react-express/client/build"));
+  app.use(express.static("client/build"));
 }
 
 //Connect to MongoDB >>googleBooks should be created, check robo3t
